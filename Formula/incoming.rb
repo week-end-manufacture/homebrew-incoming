b03729a184f7990a5c07d7de2a9411455a5e7ff6
42decibel <hyukzuny@gmail.com>
2024-06-02 20:57:15 +0900

    feat: Add venv

--- Formula/incoming.rb
+++ Formula/incoming.rb
@@ -1,7 +1,9 @@
 class Incoming < Formula
+    include Language::Python::Virtualenv
+
     desc "Our best file encoder"
     homepage "https://github.com/week-end-manufacture/incoming"
-    url "https://github.com/week-end-manufacture/incoming/releases/download/beta/incoming.tar.gz"
+    url "https://github.com/week-end-manufacture/incoming/archive/refs/tags/beta.tar.gz"
     sha256 "7f8be2fdbf21a342166ee0b5d0f106992be4bc3098f5f3e4218693472747e0b3"
     version "b0.0.1"
     license "MIT"
@@ -9,6 +11,9 @@ class Incoming < Formula
     depends_on "python@3.y"
 
     def install
-        virtualenv_install_with_resources
+        venv = virtualenv_create(libexec, "python3.y")
+        venv.pip_install "./requirements.txt"
+
+
     end
   end
