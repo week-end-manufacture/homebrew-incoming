a79922bc8ce5ffe88fa005b99b461ef2ca31b3dc
42decibel <hyukzuny@gmail.com>
2024-06-03 23:28:23 +0900

    fix: Update rb

--- Formula/incoming.rb
+++ Formula/incoming.rb
@@ -12,6 +12,9 @@ class Incoming < Formula
     depends_on "pkg-config"
     depends_on "libjpeg"
     depends_on "libpng"
+    depends_on "libtiff"
+    depends_on "webp"
+    depends_on "little-cms2"
 
     resource "setuptools" do
         url "https://files.pythonhosted.org/packages/d6/4f/b10f707e14ef7de524fe1f8988a294fb262a29c9b5b12275c7e188864aed/setuptools-69.5.1.tar.gz"
