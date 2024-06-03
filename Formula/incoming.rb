c36c108087fe1814bf2aa48515d8cedb0c202441
42decibel <hyukzuny@gmail.com>
2024-06-03 20:44:13 +0900

    fix: Update rb

--- Formula/incoming.rb
+++ Formula/incoming.rb
@@ -3,7 +3,7 @@ class Incoming < Formula
 
     desc "Our best file encoder"
     homepage "https://github.com/week-end-manufacture/incoming"
-    url "https://github.com/week-end-manufacture/incoming/archive/refs/tags/beta0.0.1.tar.gz"
+    url "https://github.com/week-end-manufacture/incoming/archive/refs/tags/b0.0.1.tar.gz"
     sha256 "4650bb9edbbaeda6289e537a1572c32c8ebcd921c77b0994606dbc5120e1f503"
     version "beta0.0.1"
     license "MIT"
